056c1ebde2252e2570bf9bc38688586782551d24
cabbaacb <103894445+cabbaacb@users.noreply.github.com>
2022-08-21 20:18:20 +0300

    fixed the quit action

---
+++ NetProject/Assets/Net/Scripts/Managers/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private InputAction _quit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _quit.Enable();
+    }
+
+    private void OnEnable()
+    {
+        _quit.performed += OnQuit;
+    }
+
+    private void OnQuit(InputAction.CallbackContext obj)
+    {
+#if UNITY_EDITOR        
+        print("open menu");
+        //SceneManager.LoadScene(0);
+        SceneManager.LoadScene(0);
+#elif UNITY_STANDALONE_WIN && !UNITY_EDITOR
+        SceneManager.LoadScene(0);
+
+#endif
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
