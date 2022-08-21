69b1cb6691fffc2b3e3d54d3383b272b530e8573
cabbaacb <103894445+cabbaacb@users.noreply.github.com>
2022-08-21 14:06:50 +0300

    added control system

---
+++ NetProject/Assets/Net/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private CapsuleCollider _collider;
+    [SerializeField] private bool _player1;
+    [SerializeField, Range(1f, 10f)] private float _moveSpeed = 3f;
+
+    private float _maxSpeed = 3f;
+
+
+    private Controls _controls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = GetComponent<CapsuleCollider>();
+        _rigidbody = GetComponent<Rigidbody>();
+
+        _controls = new Controls();
+        _controls.Player1.Enable();
+        _controls.Player2.Enable();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var direction = _player1
+            ? _controls.Player1.Movement.ReadValue<Vector2>()
+            : _controls.Player2.Movement.ReadValue<Vector2>();
+
+        if (direction.x == 0 && direction.y == 0) return;
+
+        var velocity = _rigidbody.velocity;
+
+        velocity += new Vector3(direction.y, 0f, direction.x) * _moveSpeed * Time.fixedDeltaTime;
+
+        velocity.y = 0f;
+        velocity = Vector3.ClampMagnitude(velocity, _maxSpeed);
+    }
+
+
+    private void OnDestroy()
+    {
+        _controls.Player1.Disable();
+        _controls.Player2.Disable();
+    }
+}
---
+++ NetProject/Assets/Net/Scripts/ProjectileController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Net
+{
+    public class ProjectileController : MonoBehaviour
+    {
+        [SerializeField, Range(1f, 10f)] private float _moveSpeed = 3f;
+        [SerializeField, Range(1f, 10f)] private float _damage = 1f;
+        [SerializeField, Range(1f, 15f)] private float _lifetime = 7f;
+
+
+        private void OnEnable()
+        {
+            StartCoroutine(OnDie());
+        }
+
+
+        public float GetDamage => _damage;
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position += transform.forward * _moveSpeed * Time.deltaTime;
+        }
+
+        private IEnumerator OnDie()
+        {
+            yield return new WaitForSeconds(_lifetime);
+            Destroy(gameObject);
+        }
+    }
+}
